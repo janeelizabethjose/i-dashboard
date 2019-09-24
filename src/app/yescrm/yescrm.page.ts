c9815039a61b8822fd6a4527cf874c49004fc436
Jane ELizabeth Jose <janeelizabeth.jose2@xchanginging.com>
2019-09-24 19:39:48 +0800

    added new comment for testing

--- src/app/yescrm/yescrm.page.ts
+++ src/app/yescrm/yescrm.page.ts
@@ -69,6 +69,7 @@ barChartMethod() {
   }
 
   doughnutChartMethod() {
+    // change later
     this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
       type: 'doughnut',
       data: {
